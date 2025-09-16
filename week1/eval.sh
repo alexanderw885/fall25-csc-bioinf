7b286c6e66fe8da3f82a36768ef3194591403352
Alex <alexanderw@uvic.ca>
2025-09-16 16:03:04 -0700

    fixed filepath

--- week1/eval.sh
+++ week1/eval.sh
@@ -1,5 +1,5 @@
 set -euxo pipefail
 export PATH=${PATH}:${HOME}/.codon/bin
 
-test=$(codon run ./week1/code/codon/main.py data/data1)
+test=$(codon run ./week1/code/codon/main.py ./week1/data/data1)
 echo $test
